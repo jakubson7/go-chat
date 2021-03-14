464c08f2798bf043c1166b088964675aa17d2b1a
jakubson7 <kijekkuba2006@gmail.com>
2021-03-14 22:11:48 +0100

    initial setup

---
+++ client/src/Application.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Application = () => (
+  <div>
+    acb
+  </div>
+);
+
+export default Application;
---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './Application';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+ReactDOM.render(
+  <Application />,
+  document.getElementById('root')
+);
+
+serviceWorkerRegistration.register();
